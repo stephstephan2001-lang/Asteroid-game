3ae89d902909eccaf2e150396ab2aaec6bb59672
Stefan <stephstephan2001@gmail.com>
2026-03-22 19:55:02 +0200

    Added player and asteroids, no collisions yet

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
+from asteroidfield import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player  
+from asteroid import Asteroid
 
 
 def main():
@@ -10,13 +13,41 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    clock = pygame.time.Clock()
+    dt = 0
+    
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    
+    asteroids = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    AsteroidField()
+    player = Player(x,y)
+
+
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    screen.fill("black")
-    pygame.display.flip()
+        dt =  clock.tick(60) / 1000.0    
+        #Fills in the screen with black
+        screen.fill("black")
+        #Updates and draws player groups
+        updatable.update(dt)
+        for drawing in drawable:
+            drawing.draw(screen)
+        #Renders the screen
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
+
